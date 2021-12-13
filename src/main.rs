0602e84ac7061eec00d80d92c3007c3e898f7ae0
Wessam Ibrahim <wessamfathi@gmail.com>
2021-12-14 00:02:14 +0200

    Trait sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+trait Speaks {
+    fn speak(&self);
+
+    fn noise(&self) -> &str;
+}
+
+trait Animal {
+    fn animal_type(&self) -> &str;
+}
+
+struct Dog {}
+
+impl Animal for Dog {
+    fn animal_type(&self) -> &str {
+        "dog"
+    }
+}
+
+impl Speaks for Dog {
+    fn speak(&self) {
+        println!("The dog said {}", self.noise());
+    }
+
+    fn noise(&self) -> &str {
+        "woof"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dog = Dog{};
+    dog.speak();
 }
